70ba7968b8ea46ba87b3be7b1a5131269ff7b31b
umidrzayev <rzayevumid079@gmail.com>
2023-12-02 20:57:41 +0400

    javascript mesele helli

---
+++ app.js
@@ -0,0 +1,148 @@
+// task8810
+// let a = 13;
+// let b = 9;
+// let c = 5;
+// let cavab;
+// function hesabla (a,b,c){
+//     cavab = a+b-c;
+// };
+// hesabla(a,b,c);
+// console.log(cavab);
+
+
+//task8812
+// let uzunluq = 7;
+// let en = 3;
+// let perimetr; 
+// let sahe; 
+// function hesabla(a,b){
+//     console.log(p = 2 * (a + b));
+//     console.log(s = a*b);
+// }
+// hesabla(uzunluq,en);
+
+// task8813
+// let uzunluq = 3;
+// let en = 2;
+// let hundurluk = 4;
+// let s;
+// let v;
+// function hesabla(a,b,c){
+//     console.log(v = a*b*c);
+//     console.log(s = 2*(a*b + a*c + b*c));
+// }
+// hesabla(uzunluq,en,hundurluk);
+
+
+// task8814
+// let teref = 3;
+// let sahe;
+// let perimetr; 
+// function hesabla(a){
+//     console.log(sahe = a**2);
+//     console.log(perimetr = 4*a);
+// }
+// hesabla(teref);
+
+
+// task 8815 
+// let teref = 3;
+// let sahe;
+// let hecm; 
+// function hesabla(a){
+//     console.log(sahe = 6*a**2);
+//     console.log(hecm = a**3);
+// }
+// hesabla(teref);
+
+
+//task 8816
+// let eded = 2; 
+// let quvvet = 10;
+// function hesabla(a,b){
+//     console.log(a**b);
+// }
+// hesabla(eded,quvvet);
+
+
+
+//task 8825
+// let x = 1;
+// let y; 
+// function hesabla(x){
+// function hesabla(x){
+//     y = x*3 - ((5*x*2)/7) + 9*x - 3/x + 1;
+//     console.log(y.toFixed(3))
+// }
+// hesabla(x);
+
+//task8827
+// let x = 1;
+// function hesabla(x){
+//    y = ((x*x+3*x-4)/(2*x-3))-((x+2)/(x*x-5*x+7));
+//    console.log(y); 
+// }
+// hesabla(x);
+
+
+//task 8831
+// let x = 1;
+// let y = -2;
+// function hesabla(x){
+//     let cavab = 2*x*x - 4*x*y + 3*y*y + (x + y)/7;
+//     console.log(cavab);
+// }
+// hesabla(x,y);
+
+
+//task 8838
+// let m = 27;
+// let n = 10;
+// let nece_eded = m/n;
+// console.log(Math.floor(nece_eded));
+
+
+//task 8840
+// let n = 27;
+// let cavab = n%10;
+// console.log(cavab);
+//?Sual 8841
+// let n = 127;
+// if(n/10 >= 1){
+//     console.log(Math.floor((n/10)%10));
+// }
+// else{
+//    console.log("Sevhlik var"); 
+// }
+
+
+//task 8844
+// let n = 127;
+// if(n/10 >= 1){
+//     let cavab = Math.floor((n/10)%10);
+//     console.log(n - cavab*10);
+// }
+// else{
+//    console.log("Sevhdir"); 
+// }
+
+
+//task 8845
+let n = 1127;
+if(n/100 >= 1){
+    console.log(Math.floor((n/100)%10));
+}
+else{
+   console.log("Sevhdir"); 
+ }
+
+//task 8849 
+// let nomre = 27;
+// if(10<=nomre && 99>=nomre){
+//     let first = Math.floor(nomre/10);
+//     let second = nomre%10;
+//     console.log(first*1000 + first*100 + second*10 + second);
+// }
+// else{
+//     console.log("Problem var!");
+// }
